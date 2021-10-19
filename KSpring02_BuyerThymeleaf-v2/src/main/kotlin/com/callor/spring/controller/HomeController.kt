17a863fe3f018a572acb32cc5035e7bc37c3a969
callor <callor@callor.com>
2021-10-19 13:42:26 +0900

    buyer

--- KSpring02_BuyerThymeleaf-v2/src/main/kotlin/com/callor/spring/controller/HomeController.kt
+++ KSpring02_BuyerThymeleaf-v2/src/main/kotlin/com/callor/spring/controller/HomeController.kt
@@ -1,12 +1,34 @@
 package com.callor.spring.controller
 
 import com.callor.spring.ConfigString
+import com.callor.spring.models.Buyer
+import com.callor.spring.service.BuyerService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
 class HomeController {
+
+    /**
+     * lateinit var
+     *  특별히 변수, 객체를 선언할때 즉시 초기화 하지 않고
+     *  나중에 초기화를 하겠다 라는 의미미
+     *
+     *  Spring 환경에서는 Component(Service, Dao 등등) 객체는
+     *  코드에서 직접 초기화 하지 않는다
+     *  코드에서는 선언만 해두고 @Autowired 설정을 하면
+     *  Spring Ioc, DI 등의 기능에 의해
+     *  (나중에, 필요할때) 자동으로 주입이 된다
+     *
+     *  반드시 lateinit 키워드를 부착하여 선언해야 한다
+     */
+    @Autowired
+    private lateinit var bService:BuyerService
 
     /**
      * Spring 일반 Controller 에서
@@ -18,11 +40,34 @@ class HomeController {
      * 문자열을 그대로 client 로 보내라
      */
     @RequestMapping(value=["/"],method=[RequestMethod.GET])
-    fun home() :String {
+    fun home(model:Model) :String {
 
         println( ConfigString.APP_NAME )
         println( ConfigString.APP_VERSION )
 
+        val userList = bService.selectAll()
+        // model.addAttribute("USERS",userList)
+        model["USERS"] = userList
+
+
         return "home"
     }
+
+    /**
+     *
+     * @ResponseBody
+     * 문자열, 객체, 배열 등등의 모든 데이터를
+     * 있는 그대로 또는 JSON 형태로 변환하여
+     * client 에 보내라
+     *
+     */
+    @ResponseBody
+    @RequestMapping(value=["/list"],method=[RequestMethod.GET])
+    fun list() : Array<Buyer> {
+        return bService.selectAll()
+    }
+
+
+
+
 }
--- KSpring02_BuyerThymeleaf-v2/src/main/kotlin/com/callor/spring/service/impl/BuyerServiceImplV1.kt
+++ KSpring02_BuyerThymeleaf-v2/src/main/kotlin/com/callor/spring/service/impl/BuyerServiceImplV1.kt
@@ -2,29 +2,60 @@ package com.callor.spring.service.impl
 
 import com.callor.spring.models.Buyer
 import com.callor.spring.service.BuyerService
+import org.springframework.stereotype.Service
+import kotlin.random.Random
 
+@Service("bServiceV1")
 class BuyerServiceImplV1:BuyerService {
 
     // 현재 BuyerServiceImplV1 에서 사용할 가상 데이터를
     // 선언하기
     // private : 여기 클래스 내부에서만 사용하는 static 변수 선언하기
     private companion object {
-        val buyerList = (
-
-                )
+        val RND = Random(System.currentTimeMillis())
+        val BUYER_LIST = arrayOf(
+            Buyer(userid="B001",
+                name="홍길동",
+                address ="서울시",
+                tel = "02-111-1234",
+                manager = "이몽룡",
+                man_tel = "010-111-1111",
+                buy_total = 10000
+            ),
+            Buyer(userid="B002",
+                name="성춘향",
+                address ="남원시",
+                tel = "042-222-2222",
+                manager = "월매",
+                man_tel = "010-222-3333",
+                buy_total = 10000
+            ),
+            Buyer(userid="B003",
+                name="장보고",
+                address ="해남시",
+                tel = "063-333-3333",
+                manager = "하히라",
+                man_tel = "010-555-5555",
+                buy_total = 10000
+            )
+        )
     }
 
 
     override fun selectAll(): Array<Buyer> {
-        TODO("Not yet implemented")
+
+        return BUYER_LIST
+
     }
 
     override fun findById(userid: String): Buyer {
-        TODO("Not yet implemented")
+        val findUser = BUYER_LIST.filter { buyer-> buyer.userid == userid }
+        return findUser[0]
     }
 
     override fun findByName(name: String): Array<Buyer> {
-        TODO("Not yet implemented")
+        val userNum = RND.nextInt(BUYER_LIST.size)
+        return arrayOf(BUYER_LIST[userNum])
     }
 
     override fun findByTel(name: String): Array<Buyer> {
